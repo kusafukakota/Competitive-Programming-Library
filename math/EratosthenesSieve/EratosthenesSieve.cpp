a85cfc57762508665716c481041620cfddcd5ed9
kusaf <47414135+kusafukakota@users.noreply.github.com>
2023-12-13 16:17:07 +0900

    Create EratosthenesSieve.cpp

---
+++ math/EratosthenesSieve/EratosthenesSieve.cpp
@@ -0,0 +1,10 @@
+vector<bool> Eratosthenes(int n) {
+  vector<bool> prime(n + 1, true);
+  if(n >= 0) { prime[0] = false; }
+  if(n >= 1) { prime[1] = false; }
+  for(int i = 2; i * i <= n; i++) {
+    if(!prime[i]) { continue; }
+    for(int j = i + i; j <= n; j += i) { prime[j] = false; }
+  }
+  return prime;
+}
