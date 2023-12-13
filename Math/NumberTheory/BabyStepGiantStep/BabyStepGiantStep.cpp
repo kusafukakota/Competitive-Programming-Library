3ee560846896265d2556b9adcd6c2fa6f2144d23
kusaf <47414135+kusafukakota@users.noreply.github.com>
2023-12-13 17:25:25 +0900

    Create BabyStepGiantStep.cpp

---
+++ Math/NumberTheory/BabyStepGiantStep/BabyStepGiantStep.cpp
@@ -0,0 +1,12 @@
+template<typename T> ll BSGS(T s, T g, ll n, ll m, const auto &f, const auto &fm) {
+  unordered_map<T, int> mp;
+  for(int i = 1; i <= m; i++) {
+    g = f(g);
+    mp[g] = -i;
+  }
+  for(int i = 1;; i++) {
+    s = fm(s);
+    if(mp.count(s)) { return m * i + mp[s]; }
+    if(n <= m * i) { return -1; }
+  }
+}
