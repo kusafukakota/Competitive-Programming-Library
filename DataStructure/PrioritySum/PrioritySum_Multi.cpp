d6e4e6c1ca3b164df5941d01d9882d981fa63891
kusaf <47414135+kusafukakota@users.noreply.github.com>
2023-12-13 17:29:53 +0900

    Create PrioritySum_Multi.cpp

---
+++ DataStructure/PrioritySum/PrioritySum_Multi.cpp
@@ -0,0 +1,58 @@
+using S = pair<ll, ll>;
+S op(S l, S r) { return {l.first + r.first, l.second + r.second}; }
+S e() { return {0, 0}; }
+ll tar;
+bool F(S x) { return tar > x.second; }
+
+struct PrioritySum {
+  ll n;
+  segtree<S, op, e> seg;
+  set<ll> s;
+  map<ll, ll> comp;
+  vector<ll> inv;
+  bool builded = false;
+  PrioritySum() {}
+  void use(ll x) {
+    assert(!builded);
+    s.insert(x);
+  }
+  void build() {
+    assert(!builded);
+    builded = true;
+    n = ssize(s);
+    seg = segtree<S, op, e>(n);
+    ll idx = 0;
+    for(auto &i : s) {
+      comp[i] = idx;
+      inv.emplace_back(i);
+      idx++;
+    }
+  }
+  void insert(ll x, ll c = 1) {
+    assert(builded);
+    auto it = comp.find(x);
+    assert(it != comp.end());
+    ll idx = it->second;
+    auto [sum, cnt] = seg.get(idx);
+    seg.set(idx, {sum + x * c, cnt + c});
+  }
+  void erase(ll x, ll c = 1) { insert(x, -c); }
+  ll min(ll k) {
+    assert(builded);
+    tar = k;
+    ll idx = seg.max_right<F>(0);
+    assert(idx <= n);
+    auto [sum, cnt] = seg.prod(0, idx);
+    sum += inv[idx] * (k - cnt);
+    return sum;
+  }
+  ll max(ll k) {
+    assert(builded);
+    tar = k;
+    ll idx = seg.min_left<F>(n) - 1;
+    assert(idx >= 0);
+    auto [sum, cnt] = seg.prod(idx, n);
+    sum -= inv[idx] * (cnt - k);
+    return sum;
+  }
+};
