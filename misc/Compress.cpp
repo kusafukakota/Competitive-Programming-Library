ba3cad104cf541ce2f88305d2d0beca420f643cc
kusaf <47414135+kusafukakota@users.noreply.github.com>
2023-12-13 10:46:45 +0900

    Create Compress.cpp

---
+++ misc/Compress.cpp
@@ -0,0 +1,7 @@
+template<typename T> vector<T> Compress(vector<T> &v) {
+  vector<T> r = v;
+  ranges::sort(r);
+  r.erase(unique(r.begin(), r.end()), r.end());
+  for(auto &i : v) {i = ranges::lower_bound(r, i) - r.begin();}
+  return r;
+}
