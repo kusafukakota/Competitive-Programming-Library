06f170d6a359697ac31a43701b2de16559deab2b
kusaf <47414135+kusafukakota@users.noreply.github.com>
2023-12-13 17:15:09 +0900

    Create MaximumRectangle.cpp

---
+++ misc/MaximumRectangle/MaximumRectangle.cpp
@@ -0,0 +1,16 @@
+pair<vector<ll>, vector<ll>> LR(vector<ll> &h) {
+  ll n = h.size();
+  vector<ll> l(n, 0), r(n, n - 1);
+  stack<pair<ll, ll>> sl, sr;
+  for(ll i = 0; i < n; i++) {
+    while(!sl.empty() && h[i] <= sl.top().first) {sl.pop();}
+    if(!sl.empty()) {l[i] = sl.top().second + 1;}
+    sl.emplace(h[i], i);
+  }
+  for(ll i = n - 1; i >= 0; i--) {
+    while(!sr.empty() && h[i] <= sr.top().first) {sr.pop();}
+    if(!sr.empty()) {r[i] = sr.top().second - 1;}
+    sr.emplace(h[i], i);
+  }
+  return {l, r};
+}
