0d408c1c39e8a7179f95566e37a990383961635a
kusaf <47414135+kusafukakota@users.noreply.github.com>
2023-12-31 19:09:37 +0900

    Create GCDConvolution.cpp

---
+++ Math/Convolution/GCDConvolution.cpp
@@ -0,0 +1,38 @@
+vector<ll> PrimeEnum(ll n) {
+  vector<bool> sieve(n / 3 + 1, true);
+  for(ll p = 5, d = 4, i = 1, sq = sqrt(n); p <= sq; p += d = 6 - d, i++) {
+    if(!sieve[i]) { continue; }
+    for(ll q = p * p / 3, r = d * p / 3 + (d * p % 3 == 2), s = 2 * p, qe = ssize(sieve); q < qe; q += r = s - r) { sieve[q] = false; }
+  }
+  vector<ll> ret = {2, 3};
+  for(ll p = 5, d = 4, i = 1; p <= n; p += d = 6 - d, i++) {
+    if(sieve[i]) { ret.emplace_back(p); }
+  }
+  while(!ret.empty() && ret.back() > n) { ret.pop_back(); }
+  return ret;
+}
+
+struct MultipleTransform {
+  template<typename T> static void zeta_transform(vector<T> &v) {
+    ll n = ssize(v) - 1;
+    for(auto &p : PrimeEnum(n)) {
+      for(ll i = n / p; i > 0; i--) { v[i] += v[i * p]; }
+    }
+  }
+  template<typename T> static void mobius_transform(vector<T> &v) {
+    ll n = ssize(v) - 1;
+    for(auto &p : PrimeEnum(n)) {
+      for(ll i = 1; i * p <= n; i++) { v[i] -= v[i * p]; }
+    }
+  }
+};
+
+template<typename T> vector<T> GCD_convolution(const vector<T> &a, const vector<T> &b) {
+  assert(ssize(a) == ssize(b));
+  auto n = ssize(a) - 1, s = a, t = b, P = PrimeEnum(n);
+  MultipleTransform::zeta_transform(s);
+  MultipleTransform::zeta_transform(t);
+  for(ll i = 0; i <= n; i++) { s[i] *= t[i]; }
+  MultipleTransform::mobius_transform(s);
+  return s;
+}
