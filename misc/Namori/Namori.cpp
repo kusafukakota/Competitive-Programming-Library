ee6e3a8c408731747ca545c704d3d930535379d5
kusaf <47414135+kusafukakota@users.noreply.github.com>
2024-02-29 12:11:25 +0900

    Create Namori.cpp

---
+++ misc/Namori/Namori.cpp
@@ -0,0 +1,18 @@
+struct Namori {
+  ll path_len;
+  vector<ll> path, loop;
+  Namori(const vector<ll> &g, ll s = 0) {
+    vector<ll> vis(g.size(), -1);
+    for(ll i = 0, p = s;; i++, p = g[p]) {
+      if(vis[p] != -1) {
+        path_len = vis[p];
+        break;
+      }
+      loop.emplace_back(p);
+      vis[p] = i;
+    }
+    for(ll i = 0; i < path_len; i++) { path.emplace_back(loop[i]); }
+    loop.erase(loop.begin(), loop.begin() + path_len);
+  }
+  inline ll operator()(ll k) const { return k < path_len ? path[k] : loop[(k - path_len) % loop.size()]; }
+};
