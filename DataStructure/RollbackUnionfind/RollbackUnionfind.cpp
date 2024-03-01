f456bcba1f8daed45f3e25b393dd65577576d011
kusaf <47414135+kusafukakota@users.noreply.github.com>
2024-03-01 13:09:26 +0900

    Update RollbackUnionfind.cpp

--- DataStructure/RollbackUnionfind/RollbackUnionfind.cpp
+++ DataStructure/RollbackUnionfind/RollbackUnionfind.cpp
@@ -1,37 +1,52 @@
-struct Rollback_Unionfind {
-  vector<ll> par;
-  stack<pair<ll, ll>> history;
-  Rollback_Unionfind(ll n): par(n, -1) {}
-  ll root(ll x) {
+template<typename S, auto op, auto e> struct RollbackUnionfind {
+  vector<int> par;
+  vector<S> val;
+  stack<tuple<int, int, S, int>> history;
+  int snap, cnt;
+  RollbackUnionfind() = default;
+  RollbackUnionfind(int n): RollbackUnionfind(vector<S>(n, e())) {}
+  RollbackUnionfind(const vector<S> &v): par(v.size(), -1), val(v), snap(0), cnt(v.size()) {}
+  int root(int x) const {
     if(par[x] < 0) { return x; }
-    else { return root(par[x]); }
+    return root(par[x]);
   }
-  void unite(ll x, ll y) {
+  void unite(int x, int y) {
     x = root(x), y = root(y);
-    history.emplace(x, par[x]);
-    history.emplace(y, par[y]);
+    history.emplace(-1, 0, e(), cnt);
+    history.emplace(x, par[x], val[x], cnt);
+    history.emplace(y, par[y], val[y], cnt);
     if(x == y) { return; }
     if(par[x] > par[y]) { swap(x, y); }
     par[x] += par[y];
     par[y] = x;
+    val[x] = op(val[x], val[y]);
+    cnt--;
   }
-  void unite(pair<ll, ll> x) { unite(x.first, x.second); }
-  bool same(ll x, ll y) { return root(x) == root(y); }
-  bool same(pair<ll, ll> x) { return same(x.first, x.second); }
-  ll size(ll x) { return -par[root(x)]; }
   void undo() {
-    for(ll i = 0; i < 2; i++) {
-      par[history.top().first] = history.top().second;
+    assert(!history.empty());
+    while(true) {
+      auto [x, p, v, c] = history.top();
       history.pop();
+      if(x == -1) { break; }
+      par[x] = p;
+      val[x] = v;
+      cnt = c;
     }
   }
-  void snapshot() {
-    while(!history.empty()) { history.pop(); }
+  bool same(int x, int y) const { return root(x) == root(y); }
+  int size(int x) const { return -par[root(x)]; }
+  int count() const { return cnt; }
+  S prod(int x) const { return val[root(x)]; }
+  void update(int x, const S &v) {
+    x = root(x);
+    history.emplace(-1, 0, e(), cnt);
+    history.emplace(x, par[x], val[x], cnt);
+    val[x] = op(val[x], v);
   }
-  void rollback() {
-    while(!history.empty()) { undo(); }
+  int state() const { return history.size(); }
+  void snapshot() { snap = history.size(); }
+  void rollback(int p = -1) {
+    if(p == -1) { p = snap; }
+    while(p < (int)history.size()) { undo(); }
   }
-  ll operator[](ll i) { return root(i); }
-  void operator()(ll x, ll y) { unite(x, y); }
-  void operator()(pair<ll, ll> x) { unite(x); }
 };
