388dfd40477358e49296aa38b237a0d0c5b7db83
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-03-03 16:52:29 +0900

    Create SubsetTransform.cpp

---
+++ Math/Convolution/_SubsetTransform/SubsetTransform.cpp
@@ -0,0 +1,18 @@
+struct SubsetTransform {
+  template<typename T> static void ZetaTransform(vector<T> &v) {
+    const int n = v.size();
+    for(int i = 1; i < n; i <<= 1) {
+      for(int j = 0; j < n; j += i << 1) {
+        for(int k = 0; k < i; k++) { v[j + k + i] += v[j + k]; }
+      }
+    }
+  }
+  template<typename T> static void MobiusTransform(vector<T> &v) {
+    const int n = v.size();
+    for(int i = 1; i < n; i <<= 1) {
+      for(int j = 0; j < n; j += i << 1) {
+        for(int k = 0; k < i; k++) { v[j + k + i] -= v[j + k]; }
+      }
+    }
+  }
+};
