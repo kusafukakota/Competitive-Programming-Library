7f4069eab6acc3e9133eb585e3bce0d6ff45c3f7
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-03-03 16:48:51 +0900

    Update OR_Convolution.cpp

--- Math/Convolution/OR_Convolution/OR_Convolution.cpp
+++ Math/Convolution/OR_Convolution/OR_Convolution.cpp
@@ -1,14 +1,14 @@
 struct SubsetTransform {
-  template<typename T> static void zeta_transform(vector<T> &v) {
-    ll n = ssize(v);
+  template<typename T> static void ZetaTransform(vector<T> &v) {
+    const int n = v.size();
     for(int i = 1; i < n; i <<= 1) {
       for(int j = 0; j < n; j += i << 1) {
         for(int k = 0; k < i; k++) { v[j + k + i] += v[j + k]; }
       }
     }
   }
-  template<typename T> static void mobius_transform(vector<T> &v) {
-    ll n = ssize(v);
+  template<typename T> static void MobiusTransform(vector<T> &v) {
+    const int n = v.size();
     for(int i = 1; i < n; i <<= 1) {
       for(int j = 0; j < n; j += i << 1) {
         for(int k = 0; k < i; k++) { v[j + k + i] -= v[j + k]; }
@@ -20,10 +20,10 @@ struct SubsetTransform {
 template<typename T> vector<T> OR_convolution(vector<T> a, vector<T> b) {
   const int n = a.size();
   assert(a.size() == b.size());
-  assert((n & (n - 1)) == 0);
-  SubsetTransform::zeta_transform(a);
-  SubsetTransform::zeta_transform(b);
+  assert(!(n & (n - 1)));
+  SubsetTransform::ZetaTransform(a);
+  SubsetTransform::ZetaTransform(b);
   for(int i = 0; i < n; i++) { a[i] *= b[i]; }
-  SubsetTransform::mobius_transform(a);
+  SubsetTransform::MobiusTransform(a);
   return a;
 }
