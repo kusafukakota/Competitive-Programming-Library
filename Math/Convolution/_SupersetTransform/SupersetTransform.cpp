1bdd0ace452d91b664fba7b2154f7d8f1c889515
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-03-03 16:52:03 +0900

    Create SupersetTransform.cpp

---
+++ Math/Convolution/_SupersetTransform/SupersetTransform.cpp
@@ -0,0 +1,18 @@
+struct SupersetTransform {
+  template<typename T> static void ZetaTransform(vector<T> &v) {
+    const int n = v.size();
+    for(int i = 1; i < n; i <<= 1) {
+      for(int j = 0; j < n; j += i << 1) {
+        for(int k = 0; k < i; k++) { v[j + k] += v[j + k + i]; }
+      }
+    }
+  }
+  template<typename T> static void MobiusTransform(vector<T> &v) {
+    const int n = v.size();
+    for(int i = 1; i < n; i <<= 1) {
+      for(int j = 0; j < n; j += i << 1) {
+        for(int k = 0; k < i; k++) { v[j + k] -= v[j + k + i]; }
+      }
+    }
+  }
+};
