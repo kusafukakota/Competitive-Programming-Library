b055cef551b282f6626fea570cbc6d64b1cd6741
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-03-04 10:24:43 +0900

    Update PrioritySum_Multi.cpp

--- DataStructure/PrioritySum/PrioritySum_Multi.cpp
+++ DataStructure/PrioritySum/PrioritySum_Multi.cpp
@@ -1,58 +1,41 @@
-using S = pair<ll, ll>;
-S op(S l, S r) { return {l.first + r.first, l.second + r.second}; }
-S e() { return {0, 0}; }
+using T = pair<ll, ll>;
+T op(T l, T r) { return {l.first + r.first, l.second + r.second}; }
+T e() { return {0, 0}; }
 ll tar;
-bool F(S x) { return tar > x.second; }
+bool F(T x) { return tar > x.second; }
 
 struct PrioritySum {
-  ll n;
-  segtree<S, op, e> seg;
-  set<ll> s;
-  map<ll, ll> comp;
-  vector<ll> inv;
-  bool built;
-  PrioritySum() : built(false){}
-  void use(ll x) {
-    assert(!built);
-    s.insert(x);
-  }
+  int n;
+  segtree<T, op, e> seg;
+  vector<ll> s;
+  PrioritySum() {}
+  void use(ll x) { s.emplace_back(x); }
   void build() {
-    assert(!built);
-    built = true;
-    n = ssize(s);
-    seg = segtree<S, op, e>(n);
-    ll idx = 0;
-    for(auto &i : s) {
-      comp[i] = idx;
-      inv.emplace_back(i);
-      idx++;
-    }
+    ranges::sort(s);
+    s.erase(unique(s.begin(), s.end()), s.end());
+    n = s.size();
+    seg = segtree<T, op, e>(n);
   }
   void insert(ll x, ll c = 1) {
-    assert(built);
-    auto it = comp.find(x);
-    assert(it != comp.end());
-    ll idx = it->second;
+    int idx = ranges::lower_bound(s, x) - s.begin();
     auto [sum, cnt] = seg.get(idx);
     seg.set(idx, {sum + x * c, cnt + c});
   }
   void erase(ll x, ll c = 1) { insert(x, -c); }
-  ll min(ll k) {
-    assert(built);
+  ll min(int k) const {
+    if(!k) { return 0; }
     tar = k;
-    ll idx = seg.max_right<F>(0);
-    assert(idx <= n);
+    int idx = seg.max_right<F>(0);
     auto [sum, cnt] = seg.prod(0, idx);
-    sum += inv[idx] * (k - cnt);
+    sum += s[idx] * (k - cnt);
     return sum;
   }
-  ll max(ll k) {
-    assert(built);
+  ll max(int k) const {
+    if(!k) { return 0; }
     tar = k;
-    ll idx = seg.min_left<F>(n) - 1;
-    assert(idx >= 0);
+    int idx = seg.min_left<F>(n) - 1;
     auto [sum, cnt] = seg.prod(idx, n);
-    sum -= inv[idx] * (cnt - k);
+    sum -= s[idx] * (cnt - k);
     return sum;
   }
 };
