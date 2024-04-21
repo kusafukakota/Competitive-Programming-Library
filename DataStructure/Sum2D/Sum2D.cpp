dcc5088940916db50eb2afaec30e7975f0247bf9
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-04-21 10:03:22 +0900

    Update Sum2D.cpp

--- DataStructure/Sum2D/Sum2D.cpp
+++ DataStructure/Sum2D/Sum2D.cpp
@@ -8,16 +8,16 @@ template<typename T> struct Sum2D {
     }
     build();
   }
-  void add(int h, int w, T z = 1) { v[h + 1][w + 1] += z; }
-  void imos_add(int h1, int w1, int h2, int w2, T z = 1) { add(h1, w1, z), add(h1, w2, -z), add(h2, w1, -z), add(h2, w2, z); }
+  void add(int x, int y, T a = 1) { v[x + 1][y + 1] += a; }
+  void imos_add(int lx, int ly, int rx, int ry, T a = 1) { add(lx, ly, a), add(lx, ry, -a), add(rx, ly, -a), add(rx, ry, a); }
   void build() {
     for(int i = 1; i < (int)v.size(); i++) {
       for(int j = 1; j < (int)v[i].size(); j++) { v[i][j] += v[i][j - 1] + v[i - 1][j] - v[i - 1][j - 1]; }
     }
   }
-  T sum(int h1, int w1, int h2, int w2) const { return v[h2][w2] - v[h1][w2] - v[h2][w1] + v[h1][w1]; }
-  T operator()(int h, int w) const { return v[h][w]; }
-  T operator()(int h1, int w1, int h2, int w2) const { return v[h2][w2] - v[h1][w2] - v[h2][w1] + v[h1][w1]; }
-  T imos_get(int h, int w) { return v[h + 1][w + 1]; }
+  T sum(int lx, int ly, int rx, int ry) const { return v[rx][ry] - v[lx][ry] - v[rx][ly] + v[lx][ly]; }
+  T operator()(int x, int y) const { return v[x][y]; }
+  T operator()(int lx, int ly, int rx, int ry) const { return v[rx][ry] - v[lx][ry] - v[rx][ly] + v[lx][ly]; }
+  T imos_get(int x, int y) { return v[x + 1][y + 1]; }
   vector<vector<T>> imos() const { return v; }
 };
