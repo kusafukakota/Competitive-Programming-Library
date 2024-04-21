274dfe1ff4a375b0f64a8ac70603e397a9e20066
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-04-21 10:37:51 +0900

    Update SegmentTree2D.cpp

--- DataStructure/SegmentTree2D/SegmentTree2D.cpp
+++ DataStructure/SegmentTree2D/SegmentTree2D.cpp
@@ -1,59 +1,56 @@
 template<typename S, auto op, auto e> struct SegTree2D {
-  static_assert(std::is_convertible_v<decltype(op), std::function<S(S, S)>>);
-  static_assert(std::is_convertible_v<decltype(e), std::function<S()>>);
-
  private:
   vector<S> seg;
   bool built;
-  int id(int h, int w) const { return h * 2 * W + w; }
-  S inner_prod(int h, int w1, int w2) {
+  int id(int x, int y) const { return x * 2 * W + y; }
+  S inner_prod(int x, int ly, int ry) {
     S res = e();
-    for(; w1 < w2; w1 >>= 1, w2 >>= 1) {
-      if(w1 & 1) { res = op(res, seg[id(h, w1)]), w1++; }
-      if(w2 & 1) { --w2, res = op(res, seg[id(h, w2)]); }
+    for(; ly < ry; ly >>= 1, ry >>= 1) {
+      if(ly & 1) { res = op(res, seg[id(x, ly)]), ly++; }
+      if(ry & 1) { --ry, res = op(res, seg[id(x, ry)]); }
     }
     return res;
   }
 
  public:
   int H, W;
-  SegTree2D(int h, int w): built(false) {
+  SegTree2D(int x, int y): built(false) {
     H = W = 1;
-    while(H < h) { H <<= 1; }
-    while(W < w) { W <<= 1; }
+    while(H < x) { H <<= 1; }
+    while(W < y) { W <<= 1; }
     seg.assign(4 * H * W, e());
   }
-  void set(int h, int w, const S &x) {
-    if(!built) { seg[id(h + H, w + W)] = x; }
+  void set(int x, int y, const S &s) {
+    if(!built) { seg[id(x + H, y + W)] = s; }
     else {
-      h += H, w += W;
-      seg[id(h, w)] = x;
-      for(int i = h >> 1; i; i >>= 1) { seg[id(i, w)] = op(seg[id(2 * i + 0, w)], seg[id(2 * i + 1, w)]); }
-      for(; h; h >>= 1) {
-        for(int j = w >> 1; j; j >>= 1) { seg[id(h, j)] = op(seg[id(h, 2 * j + 0)], seg[id(h, 2 * j + 1)]); }
+      x += H, y += W;
+      seg[id(x, y)] = s;
+      for(int i = x >> 1; i; i >>= 1) { seg[id(i, y)] = op(seg[id(2 * i, y)], seg[id(2 * i + 1, y)]); }
+      for(; x; x >>= 1) {
+        for(int j = y >> 1; j; j >>= 1) { seg[id(x, j)] = op(seg[id(x, 2 * j)], seg[id(x, 2 * j + 1)]); }
       }
     }
   }
   void build() {
     built = true;
-    for(int w = W; w < 2 * W; w++) {
-      for(int h = H - 1; h; h--) { seg[id(h, w)] = op(seg[id(2 * h + 0, w)], seg[id(2 * h + 1, w)]); }
+    for(int y = W; y < 2 * W; y++) {
+      for(int x = H - 1; x; x--) { seg[id(x, y)] = op(seg[id(2 * x, y)], seg[id(2 * x + 1, y)]); }
     }
-    for(int h = 0; h < 2 * H; h++) {
-      for(int w = W - 1; w; w--) { seg[id(h, w)] = op(seg[id(h, 2 * w + 0)], seg[id(h, 2 * w + 1)]); }
+    for(int x = 0; x < 2 * H; x++) {
+      for(int y = W - 1; y; y--) { seg[id(x, y)] = op(seg[id(x, 2 * y)], seg[id(x, 2 * y + 1)]); }
     }
   }
-  S get(int h, int w) const { return seg[id(h + H, w + W)]; }
-  S operator()(int h, int w) const { return seg[id(h + H, w + W)]; }
-  S prod(int h1, int w1, int h2, int w2) {
-    if(h1 >= h2 || w1 >= w2) { return e(); }
+  S get(int x, int y) const { return seg[id(x + H, y + W)]; }
+  S operator()(int x, int y) const { return seg[id(x + H, y + W)]; }
+  S prod(int lx, int ly, int rx, int ry) {
+    if(lx >= rx || ly >= ry) { return e(); }
     S res = e();
-    h1 += H, h2 += H, w1 += W, w2 += W;
-    for(; h1 < h2; h1 >>= 1, h2 >>= 1) {
-      if(h1 & 1) { res = op(res, inner_prod(h1, w1, w2)), h1++; }
-      if(h2 & 1) { --h2, res = op(res, inner_prod(h2, w1, w2)); }
+    lx += H, rx += H, ly += W, ry += W;
+    for(; lx < rx; lx >>= 1, rx >>= 1) {
+      if(lx & 1) { res = op(res, inner_prod(lx, ly, ry)), lx++; }
+      if(rx & 1) { --rx, res = op(res, inner_prod(rx, ly, ry)); }
     }
     return res;
   }
-  S operator()(int h1, int w1, int h2, int w2) { return prod(h1, w1, h2, w2); }
+  S operator()(int lx, int ly, int rx, int ry) { return prod(lx, ly, rx, ry); }
 };
