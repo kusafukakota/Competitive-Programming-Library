dca6b7d55afbc2403a168fb0ae609a56cf713eae
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-04-30 18:35:30 +0900

    Update CompressedSegmentTree.cpp

--- DataStructure/CompressedSegmentTree/CompressedSegmentTree.cpp
+++ DataStructure/CompressedSegmentTree/CompressedSegmentTree.cpp
@@ -1,15 +1,17 @@
 template<typename T, typename S, auto op, auto e> struct CompressedSegTree {
  private:
   segtree<S, op, e> seg;
-  vector<T> s;
-  int id(T x) { return ranges::lower_bound(s, x) - s.begin(); }
+  vector<T> p;
+  int id(T x) { return ranges::lower_bound(p, x) - p.begin(); }
 
  public:
-  void use(ll x) { s.emplace_back(x); }
+  CompressedSegTree() = default;
+  CompressedSegTree(int N) { p.reserve(N); }
+  void use(ll x) { p.emplace_back(x); }
   void build() {
-    ranges::sort(s);
-    s.erase(unique(s.begin(), s.end()), s.end());
-    seg = segtree<S, op, e>(s.size());
+    ranges::sort(p);
+    p.erase(unique(p.begin(), p.end()), p.end());
+    seg = segtree<S, op, e>(p.size());
   }
   void set(T i, S x) { seg.set(id(i), x); }
   S get(T i) { return seg.get(id(i)); }
