1896a8066770233d0c228f9bc4ee8e5cf2ff77af
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-04-30 18:59:25 +0900

    Update CompressedRectangleSum.cpp

--- DataStructure/CompressedRectangleSum/CompressedRectangleSum.cpp
+++ DataStructure/CompressedRectangleSum/CompressedRectangleSum.cpp
@@ -14,7 +14,7 @@ template<typename S, typename T> struct CompressedRectangleSum {
     Qs.reserve(Q);
   }
   void add(S x, S y, T w) { ps.emplace_back(P{x, y, w}); }
-  void sum(S lx, S ly, S rx, S ry) { Qs.emplace_back(Q1{lx, ly, rx, ry}); }
+  void sum(S lx, S rx, S ly, S ry) { Qs.emplace_back(Q1{lx, ly, rx, ry}); }
   vector<T> solve() {
     int N = ps.size(), Q = Qs.size();
     ranges::sort(ps, [](const P &a, const P &b) { return a.y < b.y; });
@@ -33,23 +33,23 @@ template<typename S, typename T> struct CompressedRectangleSum {
     vector<Q2> qs;
     qs.reserve(Q + Q);
     for(int i = 0; i < Q; i++) {
-      auto &query = Qs[i];
-      int d = ranges::lower_bound(ys, query.ly) - ys.begin(), u = ranges::lower_bound(ys, query.ry) - ys.begin();
-      qs.emplace_back(Q2{query.lx, d, u, false, i});
-      qs.emplace_back(Q2{query.rx, d, u, true, i});
+      auto &q = Qs[i];
+      int d = ranges::lower_bound(ys, q.ly) - ys.begin(), u = ranges::lower_bound(ys, q.ry) - ys.begin();
+      qs.emplace_back(Q2{q.lx, d, u, false, i});
+      qs.emplace_back(Q2{q.rx, d, u, true, i});
     }
     ranges::sort(ps, [](const P &a, const P &b) { return a.x < b.x; });
     ranges::sort(qs, [](const Q2 &a, const Q2 &b) { return a.x < b.x; });
     vector<T> ans(Q);
     int j = 0;
     fenwick_tree<T> f(ys.size());
-    for(auto &query : qs) {
-      while(j < N && ps[j].x < query.x) {
+    for(auto &q : qs) {
+      while(j < N && ps[j].x < q.x) {
         f.add(ps[j].y, ps[j].w);
         j++;
       }
-      if(query.type) { ans[query.idx] += f.sum(query.ly, query.ry); }
-      else { ans[query.idx] -= f.sum(query.ly, query.ry); }
+      if(q.type) { ans[q.idx] += f.sum(q.ly, q.ry); }
+      else { ans[q.idx] -= f.sum(q.ly, q.ry); }
     }
     return ans;
   }
