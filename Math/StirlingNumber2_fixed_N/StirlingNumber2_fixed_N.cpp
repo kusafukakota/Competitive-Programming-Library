57681ad3ac0246d8a7d83821967a3d426987dc2a
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-08-16 22:18:23 +0900

    Create StirlingNumber2_fixed_N.cpp

---
+++ Math/StirlingNumber2_fixed_N/StirlingNumber2_fixed_N.cpp
@@ -0,0 +1,8 @@
+FPS<> Stirling2(int N) {
+  FPS<> f(N + 1), g(N + 1);
+  for(int i = 0; i <= N; i++) {
+    f[i] = mint(i).pow(N) * C(-i);
+    g[i] = i & 1 ? -C(-i) : C(-i);
+  }
+  return (f * g).pre(N + 1);
+}
