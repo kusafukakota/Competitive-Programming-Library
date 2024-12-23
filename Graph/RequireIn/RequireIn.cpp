c4731407b542dd7db54efe24ecdd1276c3978ea1
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2024-12-24 00:57:50 +0900

    Create RequireIn.cpp

---
+++ Graph/RequireIn/RequireIn.cpp
@@ -0,0 +1,28 @@
+template<typename T> int RequireIn(vector<vector<T>> &g_) {
+  int N = g_.size();
+  scc_graph s(N);
+  vector<vector<int>> g(N);
+  for(int i = 0; i < N; i++) {
+    for(auto &j : g_[i]) {
+      s.add_edge(i, j);
+      g[j].emplace_back(i);
+    }
+  }
+  auto scc = s.scc();
+  for(auto &i : scc) { ranges::sort(i); }
+  int r = 0;
+  for(auto &i : scc) {
+    bool in = false;
+    for(auto &j : i) {
+      for(auto &k : g[j]) {
+        if(!ranges::binary_search(i, k)) {
+          in = true;
+          break;
+        }
+      }
+      if(in) { break; }
+    }
+    if(!in) { r++; }
+  }
+  return r;
+}
