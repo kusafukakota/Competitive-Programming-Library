d93652aa93fbeba9c634fc8ac7d927c51b309576
caf_sq <47414135+kusafukakota@users.noreply.github.com>
2025-01-08 23:02:27 +0900

    Create EnumXor.cpp

---
+++ misc/Enumration/EnumXor/EnumXor.cpp
@@ -0,0 +1,17 @@
+vector<pair<ll, ll>> EnumXor(uint64_t a, uint64_t L, uint64_t R) {
+  vector<pair<ll, ll>> r;
+  for(ll i = 0; i < 64; i++) {
+    if(L == R) { break; }
+    ll b = 1LL << i;
+    if(L & b) {
+      r.emplace_back(L ^ a, (L ^ a) + b);
+      L += b;
+    }
+    if(R & b) {
+      r.emplace_back((R - b) ^ a, ((R - b) ^ a) + b);
+      R -= b;
+    }
+    if(a & b) { a ^= b; }
+  }
+  return r;
+}
